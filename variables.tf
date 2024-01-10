acdb7c0a13b8bfb70f94a22f363621c250e19ed8
srikanthanagaddala <133683841+srikanthanagaddala@users.noreply.github.com>
2024-01-10 12:06:11 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,7 +4,7 @@ variable "aws_region" {
 }
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     = "Mumbai"
+  default     = "grafana"
 }
 variable "instance_type" {
   description = "instance type for ec2"
@@ -12,12 +12,12 @@ variable "instance_type" {
 }
 variable "ami_id" {
   description = "AMI for Ubuntu Ec2 instance"
-  default     = "ami-0f5ee92e2d63afc18"
+  default     = "ami-03f4878755434977f"
 }
 variable "bucket_name" {
   description = "The name of the S3 bucket to create"
   type        = string
-  default     = "mrcloudbook-777-ajaykumar-yegireddi"
+  default     = "srikanth52"
 }
 
 variable "acl" {
